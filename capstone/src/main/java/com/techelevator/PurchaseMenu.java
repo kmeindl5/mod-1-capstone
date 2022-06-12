1158b7d7f0e53f8c1190e0a1d35279f1f4b44258
Nathan Venable <ven369.nv@gmail.com>
2022-06-12 15:36:25 -0400

    Hand off, menu naivgation works, feeder works, selection menu give null pointer exception when funds added

--- capstone/src/main/java/com/techelevator/PurchaseMenu.java
+++ capstone/src/main/java/com/techelevator/PurchaseMenu.java
@@ -2,6 +2,10 @@ package com.techelevator;
 
 public class PurchaseMenu {
 
-
+        public void purchaseMenuDisplay(){
+            System.out.println("(1) Feed Money");
+            System.out.println("(2) Select Product");
+            System.out.println("(3) Finish Transaction");
+        }
 
 }
--- capstone/src/main/java/com/techelevator/VendingMachineCLI.java
+++ capstone/src/main/java/com/techelevator/VendingMachineCLI.java
@@ -1,4 +1,5 @@
 package com.techelevator;
+import java.awt.*;
 import java.math.BigDecimal;
 import java.util.List;
 import com.techelevator.view.Menu;
@@ -12,7 +13,13 @@ public class VendingMachineCLI{
 
 	private static final String MAIN_MENU_OPTION_DISPLAY_ITEMS = "Display Vending Machine Items";
 	private static final String MAIN_MENU_OPTION_PURCHASE = "Purchase";
-	private static final String[] MAIN_MENU_OPTIONS = { MAIN_MENU_OPTION_DISPLAY_ITEMS, MAIN_MENU_OPTION_PURCHASE };
+	private static final String MAIN_MENU_OPTION_EXIT = "Exit";
+	private static final String[] MAIN_MENU_OPTIONS = { MAIN_MENU_OPTION_DISPLAY_ITEMS, MAIN_MENU_OPTION_PURCHASE , MAIN_MENU_OPTION_EXIT };
+
+	private static final String PURCHASE_MENU_FEED_MONEY = "Feed Money";
+	private static final String PURCHASE_MENU_MAKE_SELECTION = "Select Product";
+	private static final String PURCHASE_MENU_FINISH_TRANSACTION = "Finish Transaction";
+	private static final String[] PURCHASE_MENU_OPTIONS = {PURCHASE_MENU_FEED_MONEY, PURCHASE_MENU_MAKE_SELECTION, PURCHASE_MENU_FINISH_TRANSACTION};
 
 	private Menu menu;
 
@@ -28,10 +35,11 @@ public class VendingMachineCLI{
 	public void run() throws FileNotFoundException {
 		Inventory showList = new Inventory();
 		BigDecimal currentBalance = new BigDecimal(0.00);
+		boolean menuOn = true;
 
 
 
-		while (true) {
+		while (menuOn) {
 			String choice = (String) menu.getChoiceFromOptions(MAIN_MENU_OPTIONS);
 
 
@@ -62,7 +70,8 @@ public class VendingMachineCLI{
 
 			} else if (choice.equals(MAIN_MENU_OPTION_PURCHASE)) {
 				Calculator vendCalc = new Calculator();
-				Button returnPurchaseMenu = new Button ();
+				//Button returnPurchaseMenu = new Button ();
+				PurchaseMenu purchaseDisplay = new PurchaseMenu();
 
 				System.out.println((showList.getListOfProducts().get("A1").locationID) + "|"+ showList.getListOfProducts().get("A1").getName() );
 				System.out.println((showList.getListOfProducts().get("A2").locationID) + "|"+ showList.getListOfProducts().get("A2").getName() );
@@ -81,77 +90,74 @@ public class VendingMachineCLI{
 				System.out.println((showList.getListOfProducts().get("D3").locationID) + "|"+ showList.getListOfProducts().get("D3").getName() );
 				System.out.println((showList.getListOfProducts().get("D4").locationID) + "|"+ showList.getListOfProducts().get("D4").getName() );
 				System.out.println("----------------");
-				System.out.println("Current Money Provided: ");
+				System.out.println("Current Money Provided: " + currentBalance);
 				System.out.println();
+				boolean usingPurchaseMenu = true;
+
+				while(usingPurchaseMenu){String purchaseChoice = (String) menu.getChoiceFromOptions(PURCHASE_MENU_OPTIONS);
+
+					if (purchaseChoice.equals(PURCHASE_MENU_FEED_MONEY)) {
+
+						boolean usingFeeder = true;
+						System.out.println("Please insert $1.00 ,$2.00, $5.00, or $10.00: ");
+							while (usingFeeder) {
+									currentBalance = vendCalc.calcFunds(userInput, currentBalance);
+									usingFeeder = false;
+							}
+							System.out.println("Current funds: " + currentBalance);
 
 
-				System.out.println("Please make a selection: ");
-				String inputCode = userInput.nextLine();
+				} else if(purchaseChoice.equals(PURCHASE_MENU_MAKE_SELECTION)){
+						System.out.println((showList.getListOfProducts().get("A1").locationID) + "|"+ showList.getListOfProducts().get("A1").getName() );
+						System.out.println((showList.getListOfProducts().get("A2").locationID) + "|"+ showList.getListOfProducts().get("A2").getName() );
+						System.out.println((showList.getListOfProducts().get("A3").locationID) + "|"+ showList.getListOfProducts().get("A3").getName() );
+						System.out.println((showList.getListOfProducts().get("A4").locationID) + "|"+ showList.getListOfProducts().get("A4").getName() );
+						System.out.println((showList.getListOfProducts().get("B1").locationID) + "|"+ showList.getListOfProducts().get("B1").getName() );
+						System.out.println((showList.getListOfProducts().get("B2").locationID) + "|"+ showList.getListOfProducts().get("B2").getName() );
+						System.out.println((showList.getListOfProducts().get("B3").locationID) + "|"+ showList.getListOfProducts().get("B3").getName() );
+						System.out.println((showList.getListOfProducts().get("B4").locationID) + "|"+ showList.getListOfProducts().get("B4").getName() );
+						System.out.println((showList.getListOfProducts().get("C1").locationID) + "|"+ showList.getListOfProducts().get("C1").getName() );
+						System.out.println((showList.getListOfProducts().get("C2").locationID) + "|"+ showList.getListOfProducts().get("C2").getName() );
+						System.out.println((showList.getListOfProducts().get("C3").locationID) + "|"+ showList.getListOfProducts().get("C3").getName() );
+						System.out.println((showList.getListOfProducts().get("C4").locationID) + "|"+ showList.getListOfProducts().get("C4").getName() );
+						System.out.println((showList.getListOfProducts().get("D1").locationID) + "|"+ showList.getListOfProducts().get("D1").getName() );
+						System.out.println((showList.getListOfProducts().get("D2").locationID) + "|"+ showList.getListOfProducts().get("D2").getName() );
+						System.out.println((showList.getListOfProducts().get("D3").locationID) + "|"+ showList.getListOfProducts().get("D3").getName() );
+						System.out.println((showList.getListOfProducts().get("D4").locationID) + "|"+ showList.getListOfProducts().get("D4").getName() );
+						System.out.println("Please enter the product slot location: ");
+						System.out.println(currentBalance);
+						String location = userInput.nextLine();
+						location = location.toUpperCase();
 
-				if(inputCode.equals("1")){
-					boolean usingFeeder = true;
+						if(location.equalsIgnoreCase(showList.getListOfProducts().get(location).getLocationID())){
 
+							System.out.println("Dispensing selected product >>> " + location +" " +  showList.getListOfProducts().get(location).getName());
+							System.out.println("Current remaining funds: " + vendCalc.calcChange(currentBalance));
+							System.out.println(currentBalance);
 
-					System.out.println("Please insert $1.00 ,$2.00, $5.00, or $10.00: ");
-					while (usingFeeder) {
-						currentBalance= vendCalc.calcFunds(userInput, currentBalance);
-						System.out.println("Current funds: " + currentBalance);
-
-						System.out.println(returnPurchaseMenu.buttonDisplay());
-
-						System.out.println("Please make a selection: ");
-						String purchaseCode = userInput.nextLine();
-						userInput.nextLine();
-						if(purchaseCode.equals("1") ){
-							//System.out.println(returnPurchaseMenu);
-							System.out.println("Please make a selection: ");
-							//inputCode = userInput.nextLine();
-							usingFeeder = false;
 
 						}
 
-					}
-					System.out.println("Current funds: " + currentBalance);
 
 
-				}else if(inputCode.equals("2")){
-					System.out.println((showList.getListOfProducts().get("A1").locationID) + "|"+ showList.getListOfProducts().get("A1").getName() );
-					System.out.println((showList.getListOfProducts().get("A2").locationID) + "|"+ showList.getListOfProducts().get("A2").getName() );
-					System.out.println((showList.getListOfProducts().get("A3").locationID) + "|"+ showList.getListOfProducts().get("A3").getName() );
-					System.out.println((showList.getListOfProducts().get("A4").locationID) + "|"+ showList.getListOfProducts().get("A4").getName() );
-					System.out.println((showList.getListOfProducts().get("B1").locationID) + "|"+ showList.getListOfProducts().get("B1").getName() );
-					System.out.println((showList.getListOfProducts().get("B2").locationID) + "|"+ showList.getListOfProducts().get("B2").getName() );
-					System.out.println((showList.getListOfProducts().get("B3").locationID) + "|"+ showList.getListOfProducts().get("B3").getName() );
-					System.out.println((showList.getListOfProducts().get("B4").locationID) + "|"+ showList.getListOfProducts().get("B4").getName() );
-					System.out.println((showList.getListOfProducts().get("C1").locationID) + "|"+ showList.getListOfProducts().get("C1").getName() );
-					System.out.println((showList.getListOfProducts().get("C2").locationID) + "|"+ showList.getListOfProducts().get("C2").getName() );
-					System.out.println((showList.getListOfProducts().get("C3").locationID) + "|"+ showList.getListOfProducts().get("C3").getName() );
-					System.out.println((showList.getListOfProducts().get("C4").locationID) + "|"+ showList.getListOfProducts().get("C4").getName() );
-					System.out.println((showList.getListOfProducts().get("D1").locationID) + "|"+ showList.getListOfProducts().get("D1").getName() );
-					System.out.println((showList.getListOfProducts().get("D2").locationID) + "|"+ showList.getListOfProducts().get("D2").getName() );
-					System.out.println((showList.getListOfProducts().get("D3").locationID) + "|"+ showList.getListOfProducts().get("D3").getName() );
-					System.out.println((showList.getListOfProducts().get("D4").locationID) + "|"+ showList.getListOfProducts().get("D4").getName() );
-					System.out.println("Please enter the product slot location: ");
-					String location = userInput.nextLine();
-					location = location.toUpperCase();
 
-					if(location.equalsIgnoreCase(showList.getListOfProducts().get(location).getLocationID())){
-
-						System.out.println("Dispensing selected product >>> " + location +" " +  showList.getListOfProducts().get(location).getName());
-						System.out.println("Current remaining funds: " + vendCalc.calcChange(currentBalance));
-
-
+				} else if (purchaseChoice.equals(PURCHASE_MENU_FINISH_TRANSACTION)){
+						//return change function
+						usingPurchaseMenu = false;
+					} else {
+						System.out.println("Invalid Option");
 					}
 
-				}else if(inputCode.equals("3")){
 
-				}else{
-					System.out.println("Invalid Selection.");
 				}
 
 
 
 
+
+			}else if(choice.equals(MAIN_MENU_OPTION_EXIT)){
+				System.out.println("Thank You!");
+				menuOn = false;
 
 			}
 		}
